f74d32d04b1bac98a13a12aad8a64b40c753bb3f
Git_1 <manojgolla464@gmail.com>
2023-02-02 18:34:15 +0530

    first commit

---
+++ Coding practice-1/Congrats card/Congrats.js
@@ -0,0 +1,16 @@
+const element = (
+    <div className="container">
+      <div>
+        <h1>Congratulations,you have won a watch.</h1>
+      </div>
+      <div className="personCard">
+        <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+        <h1>Manoj G</h1>
+        <p>Chalapathi Institute of Engineering and Technology,Guntur.</p>
+        <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+      </div>
+    </div>
+  );
+  
+  ReactDOM.render(element, document.getElementById("root"));
+  
